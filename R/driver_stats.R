86ac867a0420d75471d807c183e14df278107a7c
hbdaarstad <hbdaarstad@ucdavis.edu>
2025-01-31 13:36:04 -0800

    Create driver_stats.R

---
+++ R/driver_stats.R
@@ -0,0 +1,22 @@
+#' Data of F1 Driver Stats 1950 - 2024
+#'
+#' The data set includes the number of points scored by race and the total wins at that point.
+#'
+#' @format A tibble with 34603 obs. and 13 variables:
+#' \describe{
+#'   \item{driverId}{ID number for the driver}
+#'   \item{raceId}{ID number for the race}
+#'   \item{driver_points}{total number of points by that race}
+#'   \item{driver_wins}{total number of points by that race}
+#'   \item{circuitId}{ID number for the circuit}
+#'   \item{race_name}{Name of the Race}
+#'   \item{year}{year}
+#'   \item{forename}{first name for the driver}
+#'   \item{surname}{Last name for the driver}
+#'   \item{driver_nationality}{Driver's registured nationality}
+#'   \item{constructorId}{ID number for the constructor}
+#'   \item{constructor}{name for the constructor}
+#'   \item{constructor_nationality}{Constructor's registured nationality}
+#' }
+#' @source \url{http://ergast.com/mrd/}
+"driver_stats"
